18a986b953269c719d517cc2405416cc03782c16
bonzofenix <bonzofenix@gmail.com>
2013-08-26 18:27:42 -0300

    minor refactors

--- app.rb
+++ app.rb
@@ -1,27 +1,27 @@
-require File.expand_path("shotgun",  File.dirname(__FILE__))
+require File.expand_path('shotgun',  File.dirname(__FILE__))
 
-require "cuba/render"
+require 'cuba/render'
 
 Cuba.plugin Cuba::Mote
 Cuba.plugin Cuba::Render
-Cuba.settings[:render][:template_engine] = "haml"
+Cuba.settings[:render][:template_engine] = 'haml'
 
 
 
 
 Cuba.use Rack::Session::Cookie,
-  key: "__insert app name__",
-  secret: "__insert secret here__"
+  key: '__insert app name__',
+  secret: '__insert secret here__'
 Cuba.use Rack::Static,
-  root: "public",
-  urls: ["/js", "/css", "/less", "/img"]
+  root: 'public',
+  urls: ['/js', '/css', '/less', '/img']
 
 Cuba.use Rack::Protection
 Cuba.use Rack::Protection::RemoteReferrer
 
 Cuba.define do
   on root do
-    res.write "hello"
+    res.write 'hello'
   end
 
   on 'session' do
--- spec/acceptance/a_user_should_be_able_to_login_spec.rb
+++ spec/acceptance/a_user_should_be_able_to_login_spec.rb
@@ -1,16 +1,16 @@
 require_relative '../helper'
 
-describe "A user should be able to login" , :type => :feature do
+describe 'A user should be able to login' , :type => :feature do
   before do
     Capybara.app = Cuba.app
   end
 
-  context "when credentials are ok" do
-    it "logins the user" do
+  context 'when credentials are ok' do
+    it 'logins the user' do
       visit '/sessions/new'
-      within("#session") do
-        fill_in 'Login', :with => 'an@example.com'
-        fill_in 'Password', :with => 'password'
+      within('#session') do
+        fill_in 'Login', with: 'an@example.com'
+        fill_in 'Password', with: 'password'
       end
 
       click_link 'Sign in'
