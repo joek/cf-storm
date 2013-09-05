cbd673b2d3d70838d2215a12cab88b47fc8b0f52
Rayko <rayko.drg@gmail.com>
2013-09-05 17:16:55 -0300

    Added some color to state and min width for containers

--- lib/apps_helper.rb
+++ lib/apps_helper.rb
@@ -1,5 +1,5 @@
 module AppsHelper
   def app_state app
-    app.started? ? 'icon-ok app-state-started' : 'icon-remove app-state-stopped'
+    app.started? ? 'icon-ok app-state-started icon-border' : 'icon-remove app-state-stopped icon-border'
   end
 end
