693b4e678c97ebde673a75c9b6279d349e734cec
gramos / rayko <ramos.gaston@gmail.com , rayko.drg@gmail.com>
2013-09-05 08:44:24 -0300

    UI improvements, Use pure css and font awesome for icons

--- test/acceptance/a_guest_should_be_redirected_to_login_on_private_content_test.rb
+++ test/acceptance/a_guest_should_be_redirected_to_login_on_private_content_test.rb
@@ -3,7 +3,7 @@ require_relative './acceptance_helper'
 scope do
   test 'when a guest tries to access private content' do
     visit '/spaces/development/apps'
-    assert has_content? 'Login with your cf user'
+    assert has_content? 'CF Storm Login'
   end
 
 end
