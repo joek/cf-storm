db4d85c0b432f89ea6b072ae3dbf9c2e61dfcdc5
gramos / rayko <ramos.gaston@gmail.com , rayko.drg@gmail.com>
2013-09-11 14:05:00 -0300

    move invalid user tests to guest tests

--- test/acceptance/guest_trying_too_see_private_content_test.rb
+++ test/acceptance/guest_trying_too_see_private_content_test.rb
@@ -11,4 +11,16 @@ scope do
     end
   end
 
+  test 'when a guest with wrong credentials tries to login' do
+    visit '/sessions/new'
+    within('#new-session') do
+      fill_in 'email', with: 'invalid_mail@example.com'
+      fill_in 'password', with: 'invalid_password'
+    end
+
+    click_button 'Sign in'
+
+    assert has_content? 'Invalid credentials'
+  end
+
 end
