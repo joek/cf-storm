95ce5cd48b3427820592f6af2f9216b4cb143bb9
gramos / rayko <ramos.gaston@gmail.com , rayko.drg@gmail.com>
2013-09-13 09:29:07 -0300

    apps route refactor
    extract common behaviour  in update_with_rescue method

--- routes/apps.rb
+++ routes/apps.rb
@@ -4,11 +4,21 @@ class Apps < Cuba
     @space = current_user.spaces.find{ |s| s.name == vars[:space_name] }
     @app   = @space.apps.find{ |a| a.name == vars[:app_name] }
   end
+  
+  def update_with_rescue(exception)
+    begin
+      @app.update!
+      set_flash! 'Update successful'
+    rescue exception
+      set_flash! 'Update failed', :alert
+    end
+  end  
 
   define do
     on get do
       load_app
       @stats = @app.stats
+
       res.write view('apps/show')
     end
 
@@ -22,24 +32,16 @@ class Apps < Cuba
     on post, param('instances') do |instances|
       load_app
       @app.total_instances = instances.to_i
-      begin
-        @app.update!
-        set_flash! 'Update successful'
-      rescue CFoundry::InstancesError
-        set_flash! 'Update failed', :alert
-      end
+      update_with_rescue CFoundry::InstancesError
+
       res.redirect "/spaces/#{@space.name}/apps/#{@app.name}"
     end
 
     on post, param('memory') do |memory|
       load_app
       @app.memory = memory.to_i
-      begin
-        @app.update!
-        set_flash! 'Update successful'
-      rescue CFoundry::AppMemoryQuotaExceeded
-        set_flash! 'Update failed', :alert
-      end
+      update_with_rescue CFoundry::AppMemoryQuotaExceeded
+ 
       res.redirect "/spaces/#{@space.name}/apps/#{@app.name}"
     end
 
