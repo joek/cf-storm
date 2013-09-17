5f21d1ae4076bc46caac0f530d3449b0d9ba461b
gramos / rayko <ramos.gaston@gmail.com , rayko.drg@gmail.com>
2013-09-17 10:29:10 -0300

    call load_app just one time

--- routes/apps.rb
+++ routes/apps.rb
@@ -36,15 +36,14 @@ class Apps < Cuba
   end
 
   define do
+    
+    load_app
 
     on get, 'map_url' do
-      load_app
       res.write view('apps/map_url')
     end
 
     on get do
-      load_app
-      
       if @app.nil?
         set_flash! "The app '#{vars[:app_name]}' does not " + 
                    "exists in '#{@space.name}' space", :alert
@@ -56,14 +55,12 @@ class Apps < Cuba
     end
 
     on put, param('state') do |state|
-      load_app
       @app.started? ? @app.stop! : @app.start!
 
       res.redirect space_path @space
     end
 
     on post, param('instances') do |instances|
-      load_app
       @app.total_instances = instances.to_i
       update_with_rescue CFoundry::InstancesError
 
@@ -71,7 +68,6 @@ class Apps < Cuba
     end
 
     on post, param('memory') do |memory|
-      load_app
       @app.memory = memory.to_i
       update_with_rescue CFoundry::AppMemoryQuotaExceeded
 
@@ -79,8 +75,6 @@ class Apps < Cuba
     end
 
     on delete, param('app_name') do |app_name|
-      load_app
-
       if app_name == @app.name
         destroy_and_set_flash!
         res.redirect space_path @space
@@ -90,7 +84,6 @@ class Apps < Cuba
     end
 
     on delete do
-      load_app
       destroy_failed_and_set_flash!
     end
   end
