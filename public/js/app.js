a4076553ca567d6419ec28072b5584d991dd4683
gramos / rayko <ramos.gaston@gmail.com , rayko.drg@gmail.com>
2013-09-24 14:45:45 -0300

    added loading widget for all buttons

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,29 @@
 $(function() {
     $('.btn').click(function() {
+	if(!$(this).attr('href')){
+	    loadingMessage();	    
+	};
         $('.btn').each(function(index,value){
             if($(this).attr('href') == null){
                 $(this).attr('disabled', true);
                 $(this).parents().find('form').submit();
             };
-        });
+	});
     });
 });
+
+function loadingMessage() {
+    $.blockUI({ css: { 
+	border: 'none', 
+	padding: '15px', 
+	backgroundColor: '#000', 
+	'-webkit-border-radius': '10px', 
+	'-moz-border-radius': '10px', 
+	opacity: .5, 
+	color: '#fff',
+	'z-index': 1000
+    }}); 
+};
 
 $(function() {
     $('.trigger-unmap-confirmation').click(function() {
