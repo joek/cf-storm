c788feb6dbee22e28b490d334b0c94da02cfee0c
Rayko <rayko.drg@gmail.com>
2013-09-25 17:46:58 -0300

    Now the animation works, though a little choppy

--- public/js/app.js
+++ public/js/app.js
@@ -3,19 +3,19 @@ $(function(){
 })
 
 function loadingMessage() {
-    $.blockUI({ css: { 
-	border: 'none', 
-	padding: '15px', 
-	backgroundColor: '#000', 
-	'-webkit-border-radius': '10px', 
-	'-moz-border-radius': '10px', 
-	opacity: .5, 
-	color: '#fff',
-	'font-size': '30px',
-	'z-index': 1000 },
+    $.blockUI({ css: {
+        border: 'none',
+        padding: '15px',
+        backgroundColor: '#000',
+        '-webkit-border-radius': '10px',
+        '-moz-border-radius': '10px',
+        opacity: .5,
+        color: '#fff',
+        'font-size': '30px',
+        'z-index': 1000 },
         message: '<i class="icon-spinner icon-spin icon-large"></i> Loading...'
-		
-	      }); 
+
+              });
 };
 
 $(function() {
@@ -38,9 +38,15 @@ $(function() {
 });
 
 $(function() {
-    var current = parseInt($('#health-current').attr('value'));
-    for(var i=0; i <= current; i++) {
-	setTimeout("console.info(i)", 1);
-    }
+    var current = parseInt($('#current-health').attr('value'));
+    for(i=0; i<=current; i++){
+        updateKnob(i);
+    };
 });
 
+function updateKnob(value){
+    window.setTimeout(function(){
+        $('#health-monitor').val(value).trigger('change');
+        console.info(value);
+    }, (300+(value*20)));
+};
