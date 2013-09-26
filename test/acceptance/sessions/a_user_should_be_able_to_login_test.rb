32f0a33b9355908e11fd6218fee443da6d9ec8fc
gramos / rayko <ramos.gaston@gmail.com , rayko.drg@gmail.com>
2013-09-26 15:14:28 -0300

    Refactor of session tests

--- test/acceptance/sessions/a_user_should_be_able_to_login_test.rb
+++
@@ -1,7 +0,0 @@
-scope do
-  test 'when credentials are ok logins the user' do
-    login_user!
-    avatar = page.find('#user-avatar')
-    assert avatar[:title] == "You are logged in as #{Settings::API_TEST_USERNAME}"
-  end
-end
--- test/acceptance/sessions/trying_too_see_private_content_test.rb
+++ test/acceptance/sessions/trying_too_see_private_content_test.rb
@@ -29,4 +29,10 @@ scope do
     
     assert has_content? 'Invalid credentials'
   end  
+
+  test 'when credentials are ok logins the user' do
+    login_user!
+    avatar = page.find('#user-avatar')
+    assert avatar[:title] == "You are logged in as #{Settings::API_TEST_USERNAME}"
+  end
 end
