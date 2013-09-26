3e241a7f8bd141f9d633492cd40e497738dd6533
gramos / rayko <ramos.gaston@gmail.com , rayko.drg@gmail.com>
2013-09-26 15:23:21 -0300

    Session tests descriptions refactored

--- test/acceptance/sessions/trying_too_see_private_content_test.rb
+++ test/acceptance/sessions/trying_too_see_private_content_test.rb
@@ -1,17 +1,19 @@
 scope do
-  test 'when a guest tries to access private content' do
+
+  # Context: visitor
+  test 'I try visit private section so I get back to login' do
     visit '/spaces/development/apps'
     assert page.find '#new-session'
   end
 
-  test "a guest shouldn't see main menu" do
+  test "I try to see private content, main menu" do
     visit '/'
     assert_raise Capybara::ElementNotFound do
       page.find '#nav-menu'
     end
   end
 
-  test 'when a guest with wrong credentials tries to login' do
+  test 'I try to login with fake credentials so I get an error msg' do
     visit '/sessions/new'
     within('#new-session') do
       fill_in 'email', with: 'invalid_mail@example.com'
@@ -23,14 +25,17 @@ scope do
     assert has_content? 'Invalid credentials'
   end
   
-  test 'when a guest does not fill the username and pass' do
+  test 'I try to log in with no credentials so I get an error msg' do
     visit '/sessions/new'
     click_button 'Sign in'
     
     assert has_content? 'Invalid credentials'
   end  
 
-  test 'when credentials are ok logins the user' do
+
+  # ----------------------------------------------------------------------------
+  # Context: A real user
+  test 'I login with real credentials' do
     login_user!
     avatar = page.find('#user-avatar')
     assert avatar[:title] == "You are logged in as #{Settings::API_TEST_USERNAME}"
