73f034dcb78fb0df9da3fa6f8c4aed09c12a130b
gramos / rayko <ramos.gaston@gmail.com , rayko.drg@gmail.com>
2013-10-01 11:30:21 -0300

    rollback refactoring

--- routes/apps.rb
+++ routes/apps.rb
@@ -1,4 +1,74 @@
 class Apps < Cuba
+
+  def update_with_rescue
+    begin
+      @app.update!
+      set_flash! 'Update successful'
+    rescue  CFoundry::InstancesError
+      set_flash! 'Update failed', :alert
+    rescue CFoundry::AppMemoryQuotaExceeded
+      set_flash! "Update failed: You have exceeded your organization's memory limit", :alert
+    end
+  end
+
+  def destroy_and_set_flash!
+    routes = @app.routes
+
+    if @app.delete
+      routes.each{|r| r.delete}
+      set_flash! "#{@app.name} destroyed"
+    else
+      set_flash! "#{@app.name} was not destroyed, a problem occured"
+    end
+  end
+
+  def destroy_failed_and_set_flash! app_name=nil
+    if app_name
+      set_flash! "\"#{app_name}\" and \"#{@app.name}\"" +
+        " does not match, app was not destroyed", :alert
+
+    else
+      set_flash! "No name provided for \"#{@app.name}\", " +
+        "app was not destroyed", :alert
+    end
+    res.redirect app_path @space, @app
+  end
+
+  def build_route(url, domain)
+    # TODO Review request ammounts here
+    route        = current_user.route
+    route.domain = current_user.domains_by_name(domain).first
+    route.space  = @space
+    route.host   = url
+    route
+  end
+
+  def map_url_and_set_flash! url, domain
+    route = build_route url, domain
+
+    begin
+      route.create!
+      @app.add_route route
+      set_flash! 'URL Added to the app'
+    rescue CFoundry::RouteHostTaken
+      set_flash! 'Route is already taken', :alert
+    rescue CFoundry::RouteInvalid
+      set_flash! 'Invalid URL', :alert
+    end
+  end
+
+  def remove_route_and_set_flash! route_guid
+    route = @app.routes.find{ |r| r.guid == route_guid }
+    @app.remove_route route
+    route.delete
+    set_flash! 'Route unmapped successfully'
+  end
+  
+  def load_stats_and_routes
+    @stats     = @app.stopped? ? [] : @app.stats.sort
+    @routes    = @app.routes
+  end
+
   define do
 
     load_app vars[:space_name], vars[:app_name]
@@ -10,7 +80,7 @@ class Apps < Cuba
     on get do
       if @app.nil?
         set_flash! "The app '#{vars[:app_name]}' does not " +
-                   "exists in '#{@space.name}' space", :alert
+          "exists in '#{@space.name}' space", :alert
 
         res.write view('shared/not-found')
       else
@@ -59,6 +129,7 @@ class Apps < Cuba
     on delete do
       destroy_failed_and_set_flash!
     end
+
     # Nothing matched the request address
     on default do
       res.write view('404')
