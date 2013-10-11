04574a3235301a041a6d53e52b143a63ab99d9ad
Rayko <rayko.drg@gmail.com>
2013-10-11 10:48:38 -0300

    Fixed issue of ajax stats when app is down

--- app.rb
+++ app.rb
@@ -63,8 +63,17 @@ Cuba.define do
   end
 
   on get, 'async_stats/:guid' do |guid|
-    @stats = App.rebuild(guid).stats.sort_by{|key, value| key.to_i}
-    res.write partial('apps/stats')
+    app = App.rebuild(guid)
+    if app.started?
+      @stats = App.rebuild(guid).stats.sort_by{|key, value| key.to_i}
+      res.write partial('apps/stats')
+    else
+      @stats = []
+      res.status = 503
+      res.write partial('apps/stats')
+    end
+
+
   end
 
   on root do
