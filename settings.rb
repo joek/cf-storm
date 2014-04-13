0fdb415a5893b1bdb764e21e4a1f25d86dddd706
Manuel Garcia <mgarciap@gmail.com>
2014-04-12 23:11:27 -0300

    clean up

--- settings.rb
+++ settings.rb
@@ -1,12 +1,16 @@
+require 'json'
+
 module Settings
   File.read("env.sh").scan(/(.*?)="?(.*)"?$/).each do |key, value|
     ENV[key] ||= value
   end
 
-  REDIS_URL = ENV["REDIS_URL"] 
-  HOST = ENV["APP_HOST"]
-  NAME = ENV["APP_NAME"]
+  HOST = ENV['APP_HOST']
+  NAME = ENV['APP_NAME']
   API_URL = ENV['API_URL']
   API_TEST_USERNAME = ENV['API_TEST_USERNAME']
   API_TEST_PASSWORD = ENV['API_TEST_PASSWORD']
+
+  REDIS_URL = ENV["REDIS_URL"]
+
 end
